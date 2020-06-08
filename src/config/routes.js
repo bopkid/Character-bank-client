5875a568506b6a612bf396835929fb0d3c7c86ef
alwong <alwong125@gmail.com>
2020-06-08 12:10:35 -0700

    added login fucntion

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,12 +5,19 @@ import Home from '../pages/Home'
 import CharacterList from '../pages/CharacterList'
 import CharacterShow from '../pages/CharacterShow'
 import Register from '../pages/Register'
+import Login from '../pages/Login'
 
 export default (props) =>(
     <Switch>
         {/* <Route path = "/profile" component ={ Profile } /> */}
         <Route path = '/register/' component = { Register} />
-   
+        <Route path="/login" render={ (routeComponentProps) => {
+             return <Login 
+               { ...routeComponentProps }
+               currentUser={ props.currentUser }
+               storeUser={ props.storeUser }
+             />
+    }} />
 
         <Route exact path ='/' component = { Home } />
         <Route path = '/characters/:id' component = {CharacterShow} />
