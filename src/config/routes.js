4ca8ef759e5411601b3ba1ee9a6ce4937ce81bc7
alwong <alwong125@gmail.com>
2020-06-08 11:48:14 -0700

    added register compoents

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,9 +4,14 @@ import { Switch , Route} from 'react-router-dom'
 import Home from '../pages/Home'
 import CharacterList from '../pages/CharacterList'
 import CharacterShow from '../pages/CharacterShow'
+import Register from '../pages/Register'
 
 export default (props) =>(
     <Switch>
+        {/* <Route path = "/profile" component ={ Profile } /> */}
+        <Route path = '/register/' component = { Register} />
+   
+
         <Route exact path ='/' component = { Home } />
         <Route path = '/characters/:id' component = {CharacterShow} />
         <Route path = '/characters' component = { CharacterList} />
